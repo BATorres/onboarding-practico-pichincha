84b4bd819d05f0797be7e6ef3e0fceb124adee8c
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2022-04-20 11:10:50 -0500

    Rutas para usuario y rol

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RoleComponent } from './modules/role/role/role.component';
+import { UserComponent } from './modules/user/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'rol',
+    component: RoleComponent,
+  },
+  {
+    path: 'usuario',
+    component: UserComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
