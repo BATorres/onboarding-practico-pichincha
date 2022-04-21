9b0d8adeed35ee70e8f989afb8705e396a85225e
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2022-04-21 16:38:11 -0500

    Pruebas para componentes de rol

--- src/app/modules/role/components/role-form/role-form.component.spec.ts
+++ src/app/modules/role/components/role-form/role-form.component.spec.ts
@@ -1,19 +1,36 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { RoleService } from '../../services/role.service';
 
 import { RoleFormComponent } from './role-form.component';
 
 describe('RoleFormComponent', () => {
   let component: RoleFormComponent;
   let fixture: ComponentFixture<RoleFormComponent>;
+  const service = new RoleService();
+  const validForm = {
+    id: null,
+    name: 'Rol prueba',
+  };
+  const mockActivatedRoute = {
+    snapshot: {
+      params: {
+        id: '1',
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ RoleFormComponent ],
       imports: [ FormsModule, ReactiveFormsModule, RouterModule.forRoot([]) ],
-      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+      providers: [
+        {provide: APP_BASE_HREF, useValue: '/'},
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ]
     })
     .compileComponents();
   });
@@ -26,5 +43,33 @@ describe('RoleFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Obtener id de ruta', () => {
+    expect(component.id).toBe('1');
+  });
+
+  it('Llenar formulario con parámetro de búsqueda', () => {
+    const expectedUser = {
+      id: 1,
+      name: 'Administrador',
+    };
+
+    jest.spyOn(service, 'getRole').mockImplementation(() => of([]));
+
+    component.getData();
+    expect(component.form.value).toStrictEqual(expectedUser);
+  });
+
+  it('Crear formulario válido', () => {
+    component.form.setValue({...validForm});
+
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('Crear formulario con nombre no válido', () => {
+    component.form.controls['name'].setValue('');
+
+    expect(component.form.controls['name'].errors['required']).toBeTruthy();
   });
 });
--- src/app/modules/role/components/role/role.component.spec.ts
+++ src/app/modules/role/components/role/role.component.spec.ts
@@ -1,15 +1,19 @@
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 import { SharedModule } from '../../../../shared/shared.module';
 import { SearchRolePipe } from '../../pipes/search-role.pipe';
+import { RoleService } from '../../services/role.service';
 
 import { RoleComponent } from './role.component';
 
 describe('RoleComponent', () => {
   let component: RoleComponent;
   let fixture: ComponentFixture<RoleComponent>;
+  const service = new RoleService();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,10 +33,46 @@ describe('RoleComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RoleComponent);
     component = fixture.componentInstance;
+    component = new RoleComponent(service);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Init: debe cargar los usuarios', () => {
+    const roles: Array<any> = [
+      {
+        id: 1,
+        name: 'Administrador',
+      },
+      {
+        id: 2,
+        name: 'Gerente',
+      },
+      {
+        id: 3,
+        name: 'Lider célula',
+      }
+    ];
+    
+    jest.spyOn(service, 'getAll').mockImplementation(() => of(roles));
+
+    component.ngOnInit();
+    expect( component.roles.length ).toBeGreaterThan(0);
+  });
+
+  it('deleteRole debe llamar al servicio y eliminar el rol con id 2', () => {
+    const spy = jest.spyOn(service, 'deleteRole').mockImplementation(() => of([]));
+
+    component.deleteRole(2);
+    expect(spy).toBeCalledWith(2);
+  });
+
+  it('Al dar click en el botón +Nuevo, debe redirigir a la ruta /rol/nuevo', () => {
+    let href = fixture.debugElement.query(By.css('a')).nativeElement.getAttribute('href');
+    
+    expect(href).toEqual('/rol/nuevo');
   });
 });
