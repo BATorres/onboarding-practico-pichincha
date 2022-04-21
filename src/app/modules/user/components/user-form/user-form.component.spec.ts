8f64ee375efb95d928d6f166037cc526083ebca0
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2022-04-21 16:10:15 -0500

    Pruebas para CRUD de usuario

--- src/app/modules/user/components/user-form/user-form.component.spec.ts
+++ src/app/modules/user/components/user-form/user-form.component.spec.ts
@@ -1,19 +1,38 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
+import { UserService } from '../../services/user.service';
 
 import { UserFormComponent } from './user-form.component';
 
 describe('UserFormComponent', () => {
   let component: UserFormComponent;
   let fixture: ComponentFixture<UserFormComponent>;
+  const service = new UserService();
+  const validForm = {
+    id: null,
+    name: 'Prueba',
+    email: 'prueba@hotmail.com',
+    role: 'Gerente',
+  };
+  const mockActivatedRoute = {
+    snapshot: {
+      params: {
+        id: '1',
+      }
+    }
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ UserFormComponent ],
       imports: [ FormsModule, ReactiveFormsModule, RouterModule.forRoot([]) ],
-      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+      providers: [
+        { provide: APP_BASE_HREF, useValue: '/' }, 
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ]
     })
     .compileComponents();
   });
@@ -27,4 +46,45 @@ describe('UserFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Obtener id de ruta', () => {
+    expect(component.id).toBe('1');
+  });
+
+  it('Llenar formulario con parámetro de búsqueda', () => {
+    const expectedUser = {
+      email: 'usuario1@gmail.com',
+      id: 1,
+      name: 'Usuario 1',
+      role: 'Administrador',
+    };
+
+    jest.spyOn(service, 'getUser').mockImplementation(() => of([]));
+
+    component.getData();
+    expect(component.form.value).toStrictEqual(expectedUser);
+  });
+
+  it('Crear formulario válido', () => {
+    component.form.setValue({...validForm});
+
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('Crear formulario con email no válido', () => {
+    component.form.setValue({...validForm});
+    component.form.controls['email'].setValue('usuario_prueba');
+
+    expect(component.form.controls['email'].errors['email']).toBeTruthy();
+  });
+
+  /* it('Crear usuario válido', () => {
+    component.id = undefined;
+    component.form.setValue({...validForm});
+
+    const spy = jest.spyOn(service, 'saveUser').mockImplementation(() => of([]));
+
+    component.submit();
+    expect(spy).toHaveBeenCalled();
+  }); */
 });
--- src/app/modules/user/components/user/user.component.spec.ts
+++ src/app/modules/user/components/user/user.component.spec.ts
@@ -1,15 +1,19 @@
 import { APP_BASE_HREF, CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 import { SharedModule } from '../../../../shared/shared.module';
 import { SearchUserPipe } from '../../pipes/search-user.pipe';
+import { UserService } from '../../services/user.service';
 
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
+  const service = new UserService();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -29,10 +33,46 @@ describe('UserComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(UserComponent);
     component = fixture.componentInstance;
+    component = new UserComponent(service);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Init: debe cargar los usuarios', () => {
+    const users: Array<any> = [
+      {
+        id: 1,
+        name: 'Usuario 1',
+        email: 'usuario1@gmail.com',
+        role: 'Gerente',
+      },
+      {
+        id: 2,
+        name: 'Usuario 2',
+        email: 'usuario2@gmail.com',
+        role: 'Administrador',
+      },
+    ];
+
+    jest.spyOn(service, 'getAll').mockImplementation(() => of(users));
+
+    component.ngOnInit();
+    expect( component.users.length ).toBeGreaterThan(0);
+  });
+
+  it('deleteUser debe llamar al servicio y eliminar el usuario con id 2', () => {
+    const spy = jest.spyOn(service, 'deleteUser').mockImplementation(() => of([]));
+
+    component.deleteUser(2);
+    expect(spy).toBeCalledWith(2);
+  });
+
+  it('Al dar click en el botón +Nuevo, debe redirigir a la ruta /usuario/nuevo', () => {
+    let href = fixture.debugElement.query(By.css('a')).nativeElement.getAttribute('href');
+    
+    expect(href).toEqual('/usuario/nuevo');
   });
 });
